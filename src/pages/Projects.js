4f99325b1e739a0abae8c9e4bf5bf9c3798af108
Markus Maelzer <markus.maelzer@gmail.com>
2019-07-09 16:16:54 +0200

    add temporary cms integration

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { InView } from 'react-intersection-observer';
+import axios from 'axios';
 
 import {
   Title, ScrollSlider,
@@ -8,10 +9,49 @@ import {
   TextFadeIn
 } from '../components';
 
+const GenerateApiUrls = function() {
+  const DOMAIN = 'http://markusmaelzer-at.stackstaging.com';
+  const ROOT_URL = '/cockpit';
+  const API_TOKEN = '?token=47b491ec7e8b7c012f69d1eeb8e417';
+  this.DOMAIN = DOMAIN;
+  this.collection = (name) => `${DOMAIN}${ROOT_URL}/api/collections/get/${name}${API_TOKEN}`;
+  this.singleton = (name) => `${DOMAIN}${ROOT_URL}/api/singletons/get/${name}${API_TOKEN}`;
+}
+
+export const API_URL = new GenerateApiUrls();
 
 
-const imgLink = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/50532ae6-dd17-46cd-832b-491558fe64ad/dag69ua-d77e67e1-942f-4ae0-bc1e-da6cad0bf6df.png/v1/fill/w_1141,h_700,q_70,strp/darker_thank_black_hei_low_poly_wallpaper_by_flapoly_dag69ua-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTgyIiwicGF0aCI6IlwvZlwvNTA1MzJhZTYtZGQxNy00NmNkLTgzMmItNDkxNTU4ZmU2NGFkXC9kYWc2OXVhLWQ3N2U2N2UxLTk0MmYtNGFlMC1iYzFlLWRhNmNhZDBiZjZkZi5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.jRo5eVKiIruXWBT5NjYqbJxQu1yGFICIAvK7zoCMZvs";
+// const imgLink = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/50532ae6-dd17-46cd-832b-491558fe64ad/dag69ua-d77e67e1-942f-4ae0-bc1e-da6cad0bf6df.png/v1/fill/w_1141,h_700,q_70,strp/darker_thank_black_hei_low_poly_wallpaper_by_flapoly_dag69ua-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTgyIiwicGF0aCI6IlwvZlwvNTA1MzJhZTYtZGQxNy00NmNkLTgzMmItNDkxNTU4ZmU2NGFkXC9kYWc2OXVhLWQ3N2U2N2UxLTk0MmYtNGFlMC1iYzFlLWRhNmNhZDBiZjZkZi5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.jRo5eVKiIruXWBT5NjYqbJxQu1yGFICIAvK7zoCMZvs";
 class Projects extends Component {
+  state = {
+    projects: [],
+  }
+  componentDidMount() {
+    axios.get(API_URL.collection('projects')).then(({data}) => {
+      this.setState({
+        projects: data.entries
+      })
+    })
+  }
+
+  renderProjects = () => {
+    return this.state.projects.map((project, i) => (
+      <InView triggerOnce  key={project._id}>
+        {({ inView, ref, entry }) => (
+          <div className="col-md-8 project" ref={ref}>
+            <Title>
+              <TextFadeIn visible={inView} timeout={600}>
+                {project.title}
+              </TextFadeIn>
+            </Title>
+            <ColorOverlay visible={inView}>
+              <img src={API_URL.DOMAIN + project.img.path} alt={project.title} />
+            </ColorOverlay>
+          </div>
+        )}
+      </InView>
+    ))
+  }
 
   render() {
     const { loaded } = this.props;
@@ -62,21 +102,8 @@ class Projects extends Component {
           </div>
         </section>
         <div className="container-big column justify-space-between">
-          <InView triggerOnce>
-            {({ inView, ref, entry }) => (
-              <div className="col-md-8 project" ref={ref}>
-                <Title>
-                  <TextFadeIn visible={inView} timeout={600}>
-                    Freetime
-                  </TextFadeIn>
-                </Title>
-                <ColorOverlay visible={inView}>
-                  <img src={imgLink} alt="" />
-                </ColorOverlay>
-              </div>
-            )}
-          </InView>
-          <InView threshold={0.3} triggerOnce>
+          {this.renderProjects()}
+          {/* <InView threshold={0.3} triggerOnce>
             {({ inView, ref, entry }) => (
               <div className="col-md-8 project" ref={ref}>
                 <Title>
@@ -89,7 +116,7 @@ class Projects extends Component {
                 </ColorOverlay>
               </div>
             )}
-          </InView>
+          </InView> */}
         </div>
       </>
     );
