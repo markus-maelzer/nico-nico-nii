a97bf385f4b41186238befa6e48e22f12ac10268
Markus Maelzer <markus.maelzer@gmail.com>
2019-09-04 16:58:47 +0200

    project-single starting 2  get shape

--- src/pages/ProjectsSingle.jsx
+++ src/pages/ProjectsSingle.jsx
@@ -30,23 +30,50 @@ class ProjectsSingle extends Component {
       );    
   }
   
-  render() {            
+  renderContent() {
+    const { content } = this.props.project;
+    if(!content) return;
+    return content.map(({ value }) => (
+      <div className="container-big row justify-space-between" key={value.title}>
+        <div className="col-md-6">
+          <img src={API_URL.DOMAIN + value.img.path} alt="" />
+        </div>
+        <div className="col-md-4">
+          <p>
+            {value.description}
+          </p>
+        </div>
+      </div>
+    ))
+  }
+
+  render() {           
+    const { project, loaded } = this.props;
+    console.log(project);
+    
+    if(!project) return <div></div>;
     return (
-      <div>
-        {/* <InView triggerOnce threshold={0.1} key={project._id}>
-        {({ inView, ref, entry }) => (
-          <>
-            <Title>
-              <TextFadeIn visible={inView && loaded} timeout={600}>
-                {project.title}
-              </TextFadeIn>
-            </Title>
-            <ColorOverlay visible={inView && loaded}>
-              <img src={API_URL.DOMAIN + project.img.path} alt={project.title} />
-            </ColorOverlay>
-          </>
-        )}
-      </InView> */}
+      <div className="project__single row justify-space-between">
+        <InView triggerOnce threshold={0.1}>
+          {({ inView, ref, entry }) => (
+            <div className="col-md-6 project" ref={ref}>
+              <Title>
+                <TextFadeIn visible={inView && loaded} timeout={600}>
+                  {project.title}
+                </TextFadeIn>
+              </Title>
+              <ColorOverlay visible={inView && loaded}>
+                <img src={API_URL.DOMAIN + project.img.path} alt={project.title} />
+              </ColorOverlay>
+            </div>
+          )}
+        </InView>        
+        <div className="col-md-4">
+          <p>
+            {project.mainText}
+          </p>
+        </div>
+        {this.renderContent()}
       </div>
     )
   }
